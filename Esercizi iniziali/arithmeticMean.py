7457fb831dcbfdffe461d76ac34878a06aade379
danielecervelli <116167804+danielecervelli@users.noreply.github.com>
2024-04-30 08:50:07 +0200

    Uploaded file

---
+++ Esercizi iniziali/arithmeticMean.py
@@ -0,0 +1,9 @@
+def arithmeticMean(n1, n2, n3) :
+    
+    # @param n1 : float
+    # @param n2 : float
+    # @param n3 : float      
+    
+    result = (n1 + n2 + n3) / 3.0
+        
+    print 'Media dei numeri inseriti:', result
---
+++ Esercizi iniziali/coneVolume.py
@@ -0,0 +1,10 @@
+def coneVolume(radius, height):
+    
+    # @param radius: float
+    # @param height: float
+    
+    radius2 = radius ** 2.0
+    result = (pi * radius2 * height) / 3.0
+    
+    print 'Volume del cono:', result    
+    
